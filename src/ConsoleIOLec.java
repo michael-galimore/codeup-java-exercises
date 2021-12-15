90fe6b0683261227b7a6efbe215b1ac7421db0cf
michael galimore <michael.galimore1@gmail.com>
2021-12-15 10:59:02 -0600

    consoleIO-lec

---
+++ src/ConsoleIOLec.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ConsoleIOLec {
+    public static void main(String[] args){
+        String name = "Mike";
+        String lastName = "G";
+        int myFavoriteNumber = 13;
+        System.out.printf("Hello my name is %s, %s.%nMy favorite number is %d", name, lastName, myFavoriteNumber);
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your fav color?");
+        String userInput = scanner.nextLine();
+        System.out.printf("Your fav color is: %s", userInput);
+
+
+
+
+    }
+}
